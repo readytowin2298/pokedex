1bedb870420dade025b005737ea61f575d7ee569
David Ames <readytowin2298@gmail.com>
2021-05-10 14:42:07 -0500

    admin

---
+++ src/js/PokeCard.js
@@ -0,0 +1,12 @@
+function PokeCard({id, name, type, base_experience}){
+    return (
+        <div className="PokeCard">
+            <h3 className="PokeCard-header">{ name }</h3>
+            <img className="PokeCard-img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} />
+            <ul className="PokeCard-text">
+                <li>Type: {type}</li>
+                <li>Experience: {base_experience}</li>
+            </ul>
+        </div>
+    )
+}
